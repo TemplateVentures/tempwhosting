1b1d939fcdcc10c127ba7c1d90b20d7825f71f77
Template Ventures <140887326+TemplateVentures@users.noreply.github.com>
2025-05-04 23:49:02 +0800

    Add files via upload

--- data_handler.php
+++ data_handler.php
@@ -15,7 +15,6 @@ if (!$responseData->success) {
 }
 
 
-
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405);
     echo json_encode(['success' => false, 'message' => 'Method not allowed']);
